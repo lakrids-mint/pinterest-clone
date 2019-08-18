8e3f6f288077d6552fb3aac8fe77e9a746e8b818
lakrids-mint <mettepernille@hotmail.com>
2019-08-19 00:29:59 +0200

    header section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import logo from "./assets/Pinterest-logo.png";
+//import SearchBar from "./components/searchBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      {/* Header */}
+      <header className="header">
+        <div className="header-container">
+          <div className="logo-container">
+            <a href="#">
+              <img src={logo} className="logo" />
+            </a>
+          </div>
+          {/* INPUT FIELD */}
+          <div className="input-container">
+            <div className="material-icons search-icon">search</div>
+            <input placeholder="Search" className="--stretch" />
+          </div>
+          {/* NAV */}
+          <nav className="ma">
+            <div className="btn-group">
+              <a href="#" className="btn-link">
+                Home
+              </a>
+              <a href="#" className="btn-link">
+                Following
+              </a>
+              <a href="#" className="btn-link">
+                @User
+              </a>
+              <a href="#" className="material-icons btn-link btn-icon">
+                textsms
+              </a>
+              <a href="#" className="material-icons btn-link btn-icon">
+                notifications
+              </a>
+              <a href="#" className="material-icons btn-link btn-icon">
+                more_horiz
+              </a>
+            </div>
+          </nav>
+        </div>
       </header>
+
+      <div className="container ">
+        <div className="card">f</div>
+        <div className="card">f</div>
+        <div className="card">f</div>
+        <div className="card">f</div>
+        <div className="card">f</div>
+      </div>
+      <button>+</button>
+      <button>?</button>
     </div>
   );
 }
---
+++ src/components/searchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const searchBar = () => {
+  return (
+    <form>
+      <input />
+    </form>
+  );
+};
+export default searchBar;
